51fa99065ad9550b09b5367dcab03e71ed8b40bc
Yue Wu <yue.wu@yale.edu>
2023-09-08 00:48:42 -0400

    add vertex address type

--- src/interface/interface.h
+++ src/interface/interface.h
@@ -1,7 +1,48 @@
-typedef struct {
-    unsigned char type: 4;
+#include "stdint.h"
+
+typedef struct
+{
+    unsigned char type : 4;
 } BroadcastMessage;
 
-typedef struct {
+typedef struct
+{
 
 } ConvergecastMessage;
+
+/*
+ * In the algorithm we always use full address to distinguish vertices between fusion partitions,
+ * but such a 64-bit full address is too large for distributed dual module logic.
+ * The dual driver should convert the full address `VertexFullAddress` to a local address `VertexAddress`
+ * and vice versa when it interacts with the distributed dual module.
+ * The dual driver needs to maintain an offset value of type `VertexFullAddress` to do this conversion.
+ */
+typedef struct
+{
+    uint32_t t;
+    uint16_t i;
+    uint16_t j;
+} VertexFullAddress;
+
+#ifndef VERTEX_T_BITS
+#define VERTEX_T_BITS 6 // t <= 64
+#endif
+
+#ifndef VERTEX_I_BITS
+#define VERTEX_I_BITS 5 // i <= 32
+#endif
+
+#ifndef VERTEX_J_BITS
+#define VERTEX_J_BITS 5 // j <= 32
+#endif
+
+#ifndef VERTEX_BITS_DATA_TYPE
+#define VERTEX_BITS_DATA_TYPE uint16_t // 6 + 5 + 5 = 16 bits
+#endif
+
+typedef struct
+{
+    VERTEX_BITS_DATA_TYPE t : VERTEX_T_BITS;
+    VERTEX_BITS_DATA_TYPE i : VERTEX_I_BITS;
+    VERTEX_BITS_DATA_TYPE j : VERTEX_J_BITS;
+} VertexAddress;
--- src/interface/tests/struct_size_test.cpp
+++ src/interface/tests/struct_size_test.cpp
@@ -4,11 +4,15 @@
 
 int main()
 {
-    if (sizeof(BroadcastMessage) > 4)
-    {
-        printf("sizeof(BroadcastMessage) = %ld\n", sizeof(BroadcastMessage));
-        throw;
-    }
+    printf("sizeof(BroadcastMessage) = %ld\n", sizeof(BroadcastMessage));
+    assert(sizeof(BroadcastMessage) <= 4);
+
+    printf("sizeof(VertexFullAddress) = %ld\n", sizeof(VertexFullAddress));
+    assert(sizeof(VertexFullAddress) == 8);
+
+    assert(VERTEX_T_BITS + VERTEX_I_BITS + VERTEX_J_BITS <= 8 * sizeof(VERTEX_BITS_DATA_TYPE));
+    printf("sizeof(VertexAddress) = %ld\n", sizeof(VertexAddress));
+    assert(sizeof(VertexAddress) == sizeof(VERTEX_BITS_DATA_TYPE));
 
     return 0;
 }
