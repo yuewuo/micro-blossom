3e65f90d01bf89fc959be14774f4a72013bf746f
Yue Wu <yue.wu@yale.edu>
2023-09-27 21:58:57 -0400

    add example spinalHDL design

---
+++ hw/spinal/microblossom/Config.scala
@@ -0,0 +1,16 @@
+package microblossom
+
+import spinal.core._
+import spinal.core.sim._
+
+object Config {
+  def spinal = SpinalConfig(
+    targetDirectory = "gen",
+    defaultConfigForClockDomains = ClockDomainConfig(
+      resetActiveLevel = HIGH
+    ),
+    onlyStdLogicVectorAtTopLevelIo = true
+  )
+
+  def sim = SimConfig.withConfig(spinal).withFstWave
+}
