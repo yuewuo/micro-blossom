95e6bb8b6bac4251b2e5232c413aa715151d6748
Yue Wu <yue.wu@yale.edu>
2024-04-04 16:09:08 -0400

    get tty result from script done

--- src/fpga/utils/get_ttyoutput.py
+++ src/fpga/utils/get_ttyoutput.py
@@ -1,15 +1,21 @@
 import argparse
 import os
+import sys
 import time
+import subprocess
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 default_ttyfile = os.path.join(script_dir, "ttymicroblossom")
 
-def get_ttyoutput(filename, exit_word = "[exit]", timeout = 180, silent = True):
-    result = ""
+def get_ttyoutput(filename, command = "", exit_word = "[exit]", timeout = 180, silent = True):
+    tty_output = ""
+    command_output = ""
     with open(filename, "r") as file:
         file.seek(0, 2) # seek to the end
+        if command != "":
+            stdout = subprocess.PIPE if silent else sys.stdout
+            child = subprocess.Popen(command, shell=True, stdout=stdout)
         last = time.time()
         while True:
             time.sleep(0.1)
@@ -18,16 +24,24 @@ def get_ttyoutput(filename, exit_word = "[exit]", timeout = 180, silent = True):
                 print(content, end='')
             if content != "":
                 last = time.time()
-            result += content
-            if time.time() - last > timeout or exit_word in result:
+            tty_output += content
+            if time.time() - last > timeout:
+                print(f"[warning] ttyoutput timeouted after {timeout}s")
                 break
-    
-    return result
+            if exit_word in tty_output:
+                break
+    if command != "":
+        child.wait()
+        if silent:
+            command_output = child.stdout.read()
+    return tty_output, command_output
 
 def main(args=None):
     parser = argparse.ArgumentParser(description='Get TTY Output, monitoring the output given a trigger command, exit word and timeout')
     parser.add_argument('-f', '--file', default=default_ttyfile,
                         help='the path of the tty output file, see README.md for more information')
+    parser.add_argument('-c', '--command', default="",
+                        help='execute this command while waiting for the file output')
     parser.add_argument('-e', '--exit-word', default="[exit]",
                         help='when seeing this word, the program is returned')
     parser.add_argument('-t', '--timeout', default="180",
@@ -41,8 +55,9 @@ def main(args=None):
     exit_word = args.exit_word
     timeout = float(args.timeout)
     silent = args.silent
+    command = args.command
 
-    return get_ttyoutput(filename, exit_word, timeout, silent)
+    return get_ttyoutput(filename, command, exit_word, timeout, silent)
 
 if __name__ == "__main__":
     main()
