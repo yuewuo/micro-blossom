732bc4bb5630c8d45ad5ad7f58576f2c0ae86d40
Yue Wu <yue.wu@yale.edu>
2024-04-09 19:08:08 -0400

    add more analysis

--- src/cpu/embedded/src/mains/benchmark_primal_dual.rs
+++ src/cpu/embedded/src/mains/benchmark_primal_dual.rs
@@ -144,4 +144,33 @@ Can't the CPU just issue 3 read transactions and let the hardware to process and
 If the CPU can be extended with registers connected to the Micro Blossom module, can we eliminate this latency or reduce it to just tens of nanoseconds? (a few clock cycles at 200MHz)
 These are open questions and doesn't seem to be solvable in the near term.
 
+The `get_conflicts` function takes 580ns. (A little bit higher that I thought, because BRAM tests say only 380ns for memory access)
+there are ~300 instructions in the `get_conflicts` function by checking the assembly code.
+Suppose the clock frequency is 3.4GHz, then this takes about 100ns to run if the pipeline is full (ideally),
+so it does make sense to have about 200ns additional time to run this function considering the branching things and stalls.
+
+look at the `ldr` instructions. Those are reading from the Micro Blossom module.
+
+ 31c:	5d75afb0 	.inst	0x5d75afb0 ; undefined
+ 320:	1bcf448f 	.inst	0x1bcf448f ; undefined
+ 324:	bdcf2445 	.inst	0xbdcf2445 ; undefined
+ 328:	bd74fbbb 	ldr	s27, [x29, #13560]
+ 32c:	def6a0c7 	.inst	0xdef6a0c7 ; undefined
+ 330:	90e4461b 	adrp	x27, ffffffffc88c0000 <binding.c.d896e387+0xffffffffc88c0000>
+ 334:	10521c28 	adr	x8, a46b8 <binding.c.d896e387+0xa46b8>
+
+
+ 394:	0420420d 	index	z13.b, #-16, #0
+ 398:	84361a05 	prfb	pldl3strm, p6, [x16, z22.s, uxtw]
+ 39c:	3ac8b1f8 	.inst	0x3ac8b1f8 ; undefined
+ 3a0:	fc471d5e 	ldr	d30, [x10, #113]!
+ 3a4:	9c542886 	ldr	q6, a88b4 <binding.c.d896e387+0xa88b4>
+ 3a8:	90b715f8 	adrp	x24, ffffffff6e2bc000 <binding.c.d896e387+0xffffffff6e2bc000>
+ 3ac:	242d2c3f 	cmpls	p15.b, p3/z, z1.b, #52
+ 3b0:	388aeb52 	ldtrsb	x18, [x26, #174]
+ 3b4:	3c55b5a5 	ldr	b5, [x13], #-165
+ 3b8:	5d88fd62 	.inst	0x5d88fd62 ; undefined
+ 3bc:	178eb818 	b	fffffffffe3ae41c <binding.c.d896e387+0xfffffffffe3ae41c>
+ 3c0:	16aec67e 	b	fffffffffabb1db8 <binding.c.d896e387+0xfffffffffabb1db8>
+
 */
