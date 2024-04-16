8a3943be3ce07bd687ea2245d20df72c2bfc24f0
Yue Wu <yue.wu@yale.edu>
2024-04-16 15:07:24 -0400

    add benchmark data

--- benchmark/behavior/offloading_rate_virtual/run.py
+++ benchmark/behavior/offloading_rate_virtual/run.py
@@ -71,6 +71,8 @@ if __name__ == "__main__":
                     command += ["--verifier", "fusion-serial"]
                     command += ["--primal-dual-type",
                                 "embedded-comb-pre-matching-virtual"]
+                    command += ["--primal-dual-config",
+                                '{"dual":{"log_instructions":true}}']
                     # command += ["--primal-dual-type", "embedded-comb"]
                     command += ["--benchmark-profiler-output",
                                 benchmark_profile_path]
@@ -83,28 +85,25 @@ if __name__ == "__main__":
                     print("\n" + stdout)
                     assert returncode == 0, "command fails..."
 
-                    print(benchmark_profile_path)
-                    profile = Profile(benchmark_profile_path)
-                    offloaded = profile.sum_offloaded()
-                    defect_num = profile.sum_defect_num()
-                    offloading_rate = 0
-                    if defect_num > 0:
-                        offloading_rate = offloaded / defect_num
-                    confidence_interval = math.sqrt(
-                        1.96 * (offloading_rate * (1. - offloading_rate) / defect_num)) / offloading_rate
-                    print(
-                        f"d {d}: defect_num {defect_num}, offloaded {offloaded}, offloading_rate: {offloading_rate}, confidence: {confidence_interval}")
-                    print_result = "%d %d %d %f %.2e\n" % (
-                        d,
-                        defect_num,
-                        offloaded,
-                        offloading_rate,
-                        confidence_interval,
-                    )
-                    results.append(print_result)
-
-            if slurm_commands_vec is not None:
-                continue
+                print(benchmark_profile_path)
+                profile = Profile(benchmark_profile_path)
+                offloaded = profile.sum_offloaded()
+                defect_num = profile.sum_defect_num()
+                offloading_rate = 0
+                if defect_num > 0:
+                    offloading_rate = offloaded / defect_num
+                confidence_interval = math.sqrt(
+                    1.96 * (offloading_rate * (1. - offloading_rate) / defect_num)) / offloading_rate
+                print(
+                    f"d {d}: defect_num {defect_num}, offloaded {offloaded}, offloading_rate: {offloading_rate}, confidence: {confidence_interval}")
+                print_result = "%d %d %d %f %.2e" % (
+                    d,
+                    defect_num,
+                    offloaded,
+                    offloading_rate,
+                    confidence_interval,
+                )
+                results.append(print_result)
 
             print("\n\n")
             print("\n".join(results))
