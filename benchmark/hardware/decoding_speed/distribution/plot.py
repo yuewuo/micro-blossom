d65f578f47ddf62e54a67e0577a51a9556627ecb
Yue Wu <yue.wu@yale.edu>
2024-06-13 17:46:05 -0400

    add plot

--- benchmark/hardware/decoding_speed/distribution/plot.py
+++ benchmark/hardware/decoding_speed/distribution/plot.py
@@ -1,16 +1,24 @@
-import os, sys, git, json
+import os, sys, git
+import matplotlib.pyplot as plt
+
 
 git_root_dir = git.Repo(".", search_parent_directories=True).working_tree_dir
 sys.path.insert(0, os.path.join(git_root_dir, "benchmark"))
 sys.path.insert(0, os.path.join(git_root_dir, "src", "fpga", "utils"))
 from main_benchmark_decoding import *
 
-names = [
-    "d_9_p_0.001_batch",
-    "d_9_p_0.001_fusion",
-]
+names = ["d_9_p_0.001_batch", "d_9_p_0.001_fusion", "d_9_p_0.001_no_offloading"]
 
 for name in names:
     with open(f"{name}.txt", "r", encoding="utf8") as f:
         latency = TimeDistribution.from_line(f.read())
-        print(latency.average_latency())
+
+        plt.cla()
+
+        # distributions
+        data = [1, 2, 3, 2, 1]
+
+        plt.hist(data)
+        plt.savefig(f"{name}.pdf")
+
+        exit(1)
