124108d41b2fc48d786bca08683e503baec0388b
Yue Wu <yue.wu@yale.edu>
2024-06-21 14:26:38 -0400

    add layer fusion offloading rate data

--- src/cpu/blossom/src/bin/paper_figures.rs
+++ src/cpu/blossom/src/bin/paper_figures.rs
@@ -1,25 +1,28 @@
 // cargo run --release --bin paper_figures
 // to render the images, use paper_micro_blossom_fusion_demo.vue
 //    in https://github.com/yuewuo/conference-talk-2023-APS-march-meeting
-//    that is, run 
+//    that is, run
 
+use fusion_blossom::dual_module::*;
+use fusion_blossom::dual_module_parallel::*;
+use fusion_blossom::dual_module_serial::*;
+use fusion_blossom::example_codes::*;
+use fusion_blossom::primal_module::*;
+use fusion_blossom::primal_module_parallel::*;
+use fusion_blossom::util::*;
+use fusion_blossom::visualize::*;
 use serde_json::json;
 
 fn main() {
     micro_fusion_demo();
+    micro_paper_code_capacity_demo();
+    micro_paper_circuit_level_demo();
+    micro_paper_phenomenological_demo();
 }
 
 const MICRO_FUSION_DEMO_RNG_SEED: u64 = 671;
 
 fn micro_fusion_demo() {
-    use fusion_blossom::dual_module::*;
-    use fusion_blossom::dual_module_parallel::*;
-    use fusion_blossom::dual_module_serial::*;
-    use fusion_blossom::example_codes::*;
-    use fusion_blossom::primal_module::*;
-    use fusion_blossom::primal_module_parallel::*;
-    use fusion_blossom::util::*;
-    use fusion_blossom::visualize::*;
     let visualize_filename = "micro_fusion_demo.json".to_string();
     let d = 5;
     let mut code = QECPlaygroundCode::new(
@@ -86,6 +89,103 @@ fn micro_fusion_demo() {
                 &perfect_matching,
                 &VisualizeSubgraph::new(&subgraph),
             ],
+        )
+        .unwrap();
+}
+
+fn micro_paper_code_capacity_demo() {
+    let visualize_filename = "micro_paper_code_capacity_demo.json".to_string();
+    let half_weight = 500;
+    let code = CodeCapacityRotatedCode::new(5, 0.1, half_weight);
+    let mut visualizer = Visualizer::new(
+        Some(visualize_data_folder() + visualize_filename.as_str()),
+        code.get_positions(),
+        true,
+    )
+    .unwrap();
+    print_visualize_link(visualize_filename);
+    // create dual module
+    let initializer = code.get_initializer();
+    let mut dual_module = DualModuleSerial::new_empty(&initializer);
+    let syndrome = SyndromePattern::new_vertices(vec![7, 10]);
+    let interface_ptr = DualModuleInterfacePtr::new_load(&syndrome, &mut dual_module);
+    let subgraph = vec![12];
+    visualizer
+        .snapshot_combined(
+            "syndrome".to_string(),
+            vec![&interface_ptr, &dual_module, &VisualizeSubgraph::new(&subgraph)],
+        )
+        .unwrap();
+}
+
+fn micro_paper_circuit_level_demo() {
+    let visualize_filename = "micro_paper_circuit_level_demo.json".to_string();
+    let d = 5;
+    let code = QECPlaygroundCode::new(
+        d,
+        0.001,
+        json!({
+            "nm": d - 1,
+            "code_type": fusion_blossom::qecp::code_builder::CodeType::RotatedPlanarCode,
+            "noise_model": fusion_blossom::qecp::noise_model_builder::NoiseModelBuilder::StimNoiseModel,
+            "qubit_type": fusion_blossom::qecp::types::QubitType::StabZ,
+            "max_half_weight": 7,
+            "trim_isolated_vertices": false,
+        }),
+    );
+    let mut visualizer = Visualizer::new(
+        Some(visualize_data_folder() + visualize_filename.as_str()),
+        code.get_positions(),
+        true,
+    )
+    .unwrap();
+    print_visualize_link(visualize_filename);
+    // create dual module
+    let initializer = code.get_initializer();
+    let mut dual_module = DualModuleSerial::new_empty(&initializer);
+    let syndrome = SyndromePattern::new_vertices(vec![45, 68]);
+    let interface_ptr = DualModuleInterfacePtr::new_load(&syndrome, &mut dual_module);
+    let subgraph = vec![149];
+    visualizer
+        .snapshot_combined(
+            "syndrome".to_string(),
+            vec![&interface_ptr, &dual_module, &VisualizeSubgraph::new(&subgraph)],
+        )
+        .unwrap();
+}
+
+fn micro_paper_phenomenological_demo() {
+    let visualize_filename = "micro_paper_phenomenological_demo.json".to_string();
+    let d = 5;
+    let code = QECPlaygroundCode::new(
+        d,
+        0.001,
+        json!({
+            "nm": d - 1,
+            "code_type": fusion_blossom::qecp::code_builder::CodeType::RotatedPlanarCode,
+            "noise_model": fusion_blossom::qecp::noise_model_builder::NoiseModelBuilder::Phenomenological,
+            "qubit_type": fusion_blossom::qecp::types::QubitType::StabZ,
+            "max_half_weight": 7,
+            "trim_isolated_vertices": false,
+        }),
+    );
+    let mut visualizer = Visualizer::new(
+        Some(visualize_data_folder() + visualize_filename.as_str()),
+        code.get_positions(),
+        true,
+    )
+    .unwrap();
+    print_visualize_link(visualize_filename);
+    // create dual module
+    let initializer = code.get_initializer();
+    let mut dual_module = DualModuleSerial::new_empty(&initializer);
+    let syndrome = SyndromePattern::new_vertices(vec![45, 68]);
+    let interface_ptr = DualModuleInterfacePtr::new_load(&syndrome, &mut dual_module);
+    let subgraph = vec![85, 95];
+    visualizer
+        .snapshot_combined(
+            "syndrome".to_string(),
+            vec![&interface_ptr, &dual_module, &VisualizeSubgraph::new(&subgraph)],
         )
         .unwrap();
 }
